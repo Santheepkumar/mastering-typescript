1d49c8054024ad8d5c0986dd767c36fff260e98e
Santheep kumar <santheep.v@skematix.in>
2021-02-27 21:15:24 +0530

    apollo server connected and type-graphql server also connected

--- reddit-server/src/index.ts
+++ reddit-server/src/index.ts
@@ -1,17 +1,33 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entries/Post";
+// import { Post } from "./entries/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const queen = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, { title: "my 3nd first post" });
-  await orm.em.persistAndFlush(post);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+  // const post = orm.em.create(Post, { title: "my 3nd first post" });
+  // await orm.em.persistAndFlush(post);
   // console.log("------------sql2-------------");
   // await orm.em.nativeInsert(post);
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  // const posts = await orm.em.find(Post, {});
+  app.listen(4000, () => {
+    console.log("Server started on localhost:4000");
+  });
 };
 
 queen().catch((err) => {
